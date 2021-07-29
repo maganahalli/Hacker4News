fba5a49a5269fffc5cf4167a9390b9c17095e825
Venky <maganahalli@gmail.com>
2021-07-29 17:20:13 -0400

    Initial Commit

---
+++ Hacker4News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker4News
+//
+//  Created by Venkatesh Maganahalli on 7/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker4News/Hacker4NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker4NewsApp.swift
+//  Hacker4News
+//
+//  Created by Venkatesh Maganahalli on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker4NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
